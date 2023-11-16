79becfde5f2c58dbc6b7da2e1c03a4c2037373e1
IHOR-IP12 <i.znetynyak.o@gmail.com>
2023-11-17 00:50:03 +0200

    My 5 lab about trible Bai-By

---
+++ test/test.py
@@ -0,0 +1,13 @@
+import unittest
+from src.main import pairs_of_tribes
+
+
+class TestCountDifferentPairs(unittest.TestCase):
+    def test_count_different_pairs(self):
+        pairs = [[3], [1, 2], [2, 4], [3, 5]]
+        result = pairs_of_tribes(pairs)
+        self.assertEqual(result, 4)
+
+
+if __name__ == "__main__":
+    unittest.main()
